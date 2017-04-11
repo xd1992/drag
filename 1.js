d50868f421bc184e8b1a63356a7da0beefe0a0fd
hh464300 <565466574@qq.com>
2017-04-11 14:35:26 +0800

    äxiugai

--- 1.js
+++ 1.js
@@ -1,4 +1,5 @@
 window.onload = function(){
 	var h = document.getElementById('h');
 	h.style.backgroundColor = 'red';
+	var index = 100;
 }
--- 2.js
+++
@@ -1,6 +0,0 @@
-alert(123);
-alert(1000);
-function getNum(){
-	console.log('abc');
-	console.log(1000);
-}
